f417dcab6ffe8364d8efcac39f65ad3f2f583be0
atindraraut <atindraraut80@gmail.com>
2022-12-23 21:21:14 +0530

    added complete responsive searchbar to the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,8 @@ import MoreIcon from "@mui/icons-material/MoreVert";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
 
 const Header = (props) => {
   const { window } = props;
@@ -61,7 +63,7 @@ const Header = (props) => {
     >
       <MenuItem>
         <IconButton
-          size="large"
+          size="medium"
           aria-label="show 17 new notifications"
           color="inherit"
         >
@@ -73,7 +75,7 @@ const Header = (props) => {
       </MenuItem>
       <MenuItem onClick={() => {}}>
         <IconButton
-          size="large"
+          size="medium"
           aria-label="account of current user"
           aria-controls="primary-search-account-menu"
           aria-haspopup="true"
@@ -107,9 +109,12 @@ const Header = (props) => {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <div style={{ height: "60px" }}>
-      <AppBar component="nav" style={{ backgroundColor: "grey",height: "60px" }}>
-        <Toolbar>
+    <div style={{ height: "50px" }}>
+      <AppBar
+        component="nav"
+        style={{ backgroundColor: "grey", height: "50px" }}
+      >
+        <Toolbar variant="dense">
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -122,42 +127,29 @@ const Header = (props) => {
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: "block" }, fontWeight:1000 }}
+            sx={{ flexGrow: 1, display: { xs: "none",sm:"block" }, fontWeight: 1000 }}
           >
             RASANlao
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
+              <Button size="medium" key={item} sx={{ color: "#f1e9e9", padding: "5px" }}>
                 {item}
               </Button>
             ))}
           </Box>
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            {/* <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search> */}
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={() => {
-                console.log("atindra");
-              }}
-              color="inherit"
-            >
+          <Search>
+            <SearchIconWrapper>
               <SearchIcon />
-            </IconButton>
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <IconButton
-              size="large"
+              size="medium"
               aria-label="show 17 new notifications"
               color="inherit"
             >
@@ -166,7 +158,7 @@ const Header = (props) => {
               </Badge>
             </IconButton>
             <IconButton
-              size="large"
+              size="medium"
               edge="end"
               aria-label="account of current user"
               aria-controls={menuId}
@@ -181,7 +173,7 @@ const Header = (props) => {
           </Box>
           <Box sx={{ display: { xs: "flex", md: "none" }, right: "0" }}>
             <IconButton
-              size="large"
+              size="medium"
               edge="end"
               aria-label="show more"
               aria-controls={mobileMenuId}
@@ -218,5 +210,47 @@ const Header = (props) => {
     </div>
   );
 };
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  height: "40px",
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 export default Header;
